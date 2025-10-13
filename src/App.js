d3e1fd6ffc7794f3db8a8dbc3d6ec7ec3d49c6dc
KauaVReis <kaua.v.reis6@aluno.senai.br>
2025-10-13 16:40:44 -0300

    input adicionado não esta 100%

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import './App.css';
 import Header from './componentes/Header/index.js';
+import Pesquisa from './componentes/Pesquisa/index.js'
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -15,6 +16,7 @@ function App() {
   return (
     <AppContainer>
         <Header/>
+        <Pesquisa />
     </AppContainer>
   );
 }
---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,12 @@
+import Input from '../Input/index.js'
+function Pesquisa() {
+    return (
+        <section>
+            <h2>Ja sabe por onde começar?</h2>
+            <h3>Encontre seu livro em nossa estante</h3>
+            <Input placeholder="Escreva sua próxima leitura:"/>
+        </section>
+    )
+}
+
+export default Pesquisa;
